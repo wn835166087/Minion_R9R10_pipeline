ece13de9db5765ccf17662324a6727e1a9207b0a
Nannnnnnn <44331857+wn835166087@users.noreply.github.com>
2023-11-26 20:07:38 -0500

    Update and rename analysis_pipeline_CN.sh to Quality_control.sh

--- Quality_control.sh
+++ Quality_control.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+
 cd NanoporeFromBRC/paper3/Li_sequencing_sup
 
-# QC for the R10.4.1 ligation sequencing
+# QC for the R10.4.1 ligation sequencing with guppy -sup basecaller
 export PYTHONPATH=/programs/nanofilt-2.8.0/lib/python3.9/site-packages
 export PATH=/programs/nanofilt-2.8.0/bin:$PATH
 # use three parallel screens to do the filtration is faster
@@ -16,7 +19,7 @@ for i in *_q10_len200.fastq.gz; do echo NanoPlot -t 8 --fastq $i --plots dot --l
 
 
 
-# QC for the R9.4.1 ligation sequencing
+# QC for the R9.4.1 ligation sequencing with guppy -sup basecaller
 cd NanoporeFromBRC/paper3/BRC_sequencing_sup
 # use parallel screens to do the filtration is faster
 gunzip -c barcode02_passMinQ7.fastq.gz | NanoFilt -q 7 -l 200 | gzip > BRC_barcode02_q7_len200.fastq.gz
